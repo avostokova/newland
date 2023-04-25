390dcd314c1e75caedc1c6129465fef8ba1d35d5
avostokova <131677243+avostokova@users.noreply.github.com>
2023-04-25 15:42:38 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-let map = L.map('map').setView([69.473434, 38.019561], 5);
+let map = L.map('map').setView([71.473434, 38.019561], 4);
 L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: 'OpenStreetMap'
